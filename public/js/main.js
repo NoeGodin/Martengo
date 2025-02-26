c58688ae76e22b28b632e6a74eeeed3cb4825ece
Noé Godin <noegodin2@gmail.com>
2025-02-26 18:28:45 +0100

    feat : reactive buttons

--- public/js/main.js
+++ public/js/main.js
@@ -4,3 +4,17 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   }),
 );
+
+document.addEventListener("DOMContentLoaded", function () {
+  const listItems = document.querySelectorAll(".block-engraving__prices li");
+
+  listItems.forEach((item) => {
+    item.addEventListener("click", function () {
+      listItems.forEach((li) => {
+        li.innerHTML = li.textContent;
+      });
+
+      this.innerHTML = `<span class="badge badge--secondary">${this.textContent}</span>`;
+    });
+  });
+});
